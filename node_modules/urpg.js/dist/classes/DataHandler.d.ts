c827a6cea27b58d506552cb23c6a9e61509af3ed
git-user-at-503503802219 <justin.m.dufresne@gmail.com>
2020-02-15 11:09:13 -0500

    removed node_modules

--- node_modules/urpg.js/dist/classes/DataHandler.d.ts
+++
@@ -1,5 +0,0 @@
-declare class DataHandler {
-    private static NULL_VALUES;
-    static castNulls<T>(data: T): T;
-}
-export { DataHandler };
--- node_modules/urpg.js/dist/classes/RequestHandler.d.ts
+++
@@ -1,5 +0,0 @@
-declare class RequestHandler {
-    static handle(url: string, method?: string): Promise<string[]>;
-    static handle<T>(url: string, method?: string): Promise<T>;
-}
-export { RequestHandler };
